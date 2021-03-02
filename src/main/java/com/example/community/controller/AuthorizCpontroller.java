ba70c225eb779406534f0e9529b54cd22d2b1c95
demonnf <2897827674@qq.com>
2021-03-02 23:18:05 +0800

    githubapp

--- src/main/java/com/example/community/controller/AuthorizCpontroller.java
+++ src/main/java/com/example/community/controller/AuthorizCpontroller.java
@@ -1,12 +1,29 @@
 package com.example.community.controller;
 
+import com.example.community.Provider.GithubProvider;
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizCpontroller {
+    @Autowired
+    private GithubProvider githubProvider;
+    @GetMapping("/callback")
 public String callback(@RequestParam(name="code") String code,
                        @RequestParam(name="state") String state){
+    AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+    accessTokenDTO.setClient_id("39aa3b52606aa47cddeb");
+    accessTokenDTO.setClient_secret("a1e9431de891cee3c1918c62dbd4acfb04192d43");
+    accessTokenDTO.setCode(code);
+    accessTokenDTO.setState(state);
+    accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+    String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+    GithubUser user = githubProvider.getUser(accessToken);
+    System.out.println(user.getName());
     return "index";
 }
 }
---
+++ src/main/java/com/example/community/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.example.community.dto;
+
+public class GithubUser {
+    public   String name;
+    public  String bio;
+    public  long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
